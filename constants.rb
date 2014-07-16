8db513531e16da43e370d398345721da77608783
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-16 22:57:50 +0100

    Added colours for numbers.

--- constants.rb
+++ constants.rb
@@ -25,5 +25,16 @@ module Minesweeper
     # Colours
 
     SILVER        = Gosu::Color.new( 0xff, 0xe0, 0xe0, 0xe0 )
+
+    BLUE1         = Gosu::Color.new( 0xff, 0x00, 0x00, 0xA0 )
+    GREEN2        = Gosu::Color.new( 0xff, 0x00, 0xA0, 0x00 )
+    RED3          = Gosu::Color.new( 0xff, 0xA0, 0x00, 0x00 )
+    PURPLE4       = Gosu::Color.new( 0xff, 0x90, 0x00, 0x90 )
+    MAROON5       = Gosu::Color.new( 0xff, 0xB0, 0x50, 0xB0 )
+    CYAN6         = Gosu::Color.new( 0xff, 0x00, 0x80, 0x80 )
+    BLACK7        = Gosu::Color::BLACK
+    GREY8         = Gosu::Color.new( 0xff, 0x50, 0x50, 0x50 )
+
+    NUMBERS       = [0, BLUE1, GREEN2, RED3, PURPLE4, MAROON5, CYAN6, BLACK7, GREY8]
   end
 end
