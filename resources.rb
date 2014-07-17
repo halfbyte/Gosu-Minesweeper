551d2906f443555f1a62adfd2be954abcd3a1c5a
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-17 17:20:35 +0100

    Resource Loader

---
+++ resources.rb
@@ -0,0 +1,21 @@
+module Minesweeper
+  class ResourceLoader
+    def initialize( game )
+      @game = game
+    end
+
+    def fonts
+      {
+        block:  Gosu::Font.new( @game, 'Arial', 20 )
+      }
+    end
+
+    def images
+      {
+        tile: Gosu::Image.new( @game, 'media/Tile.png', true ),
+        bomb: Gosu::Image.new( @game, 'media/Bomb.png', true ),
+        flag: Gosu::Image.new( @game, 'media/Flag.png', true )
+      }
+    end
+  end
+end
