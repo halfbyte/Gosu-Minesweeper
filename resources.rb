4210fd427c337202ddb778195e413e2312d588c1
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-20 10:48:38 +0100

    Added mistakenly marked bomb graphic

--- resources.rb
+++ resources.rb
@@ -13,9 +13,10 @@ module Minesweeper
 
     def images
       {
-        tile: Gosu::Image.new( @game, 'media/Tile.png', true ),
-        bomb: Gosu::Image.new( @game, 'media/Bomb.png', true ),
-        flag: Gosu::Image.new( @game, 'media/Flag.png', true )
+        tile:     Gosu::Image.new( @game, 'media/Tile.png', true ),
+        bomb:     Gosu::Image.new( @game, 'media/Bomb.png', true ),
+        flag:     Gosu::Image.new( @game, 'media/Flag.png', true ),
+        not_bomb: Gosu::Image.new( @game, 'media/NotBomb.png', true )
       }
     end
   end
